b1c0c9070a7897278f2c2eb876bbde34eb3ce586
Rohit-Gurung <rohtitgurung4848@gmail.com>
2022-01-27 18:36:42 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,38 +1,45 @@
 function computerPlay() {
   let choicesArray = ["rock", "paper", "scissors"];
   let computerChoice = Math.floor(Math.random() * choicesArray.length);
-
+  console.log(computerChoice);
+  console.log(choicesArray[computerChoice]);
   if (computerChoice === 1) {
-    return choicesArray[0];
+    return choicesArray[computerChoice];
   } else if (computerChoice === 2) {
-    return choicesArray[1];
+    return choicesArray[computerChoice];
   }
-  return choicesArray[2];
+  return choicesArray[computerChoice];
 }
 
 let computerSelection = computerPlay();
-let playerChoice = prompt("Rock paper or scissors", "");
-if (playerChoice === "" || playerChoice === null) alert("Invalid Entry");
-let playerSelection = playerChoice.trim().toLowerCase();
+let playerChoice = prompt("Enter Rock Paper or Scissors", "");
+
+if (playerChoice) {
+  playerSelection = playerChoice.trim().toLowerCase();
+} else {
+  alert("Must be Rock Paper or Scissors");
+}
 if (
   playerSelection === "rock" ||
   playerSelection === "paper" ||
   playerSelection === "scissors"
 ) {
-} else playerSelection === "" || playerSelection === null;
+  playerSelection;
+} else {
+  alert("Invalid!!!");
+  delete playerSelection;
+}
 
 function playRound(playerSelection, computerSelection) {
   let results =
     playerSelection === computerSelection
-      ? `Player : ${playerSelection} Computer : ${computerSelection} Its a tie`
+      ? `Player : ${playerSelection}\nComputer : ${computerSelection}\nIts a tie`
       : playerSelection === "rock" && computerSelection === "paper"
-      ? `Player : ${playerSelection} Computer : ${computerSelection} Computer Wins`
+      ? `Player : ${playerSelection}\nComputer : ${computerSelection}\nComputer Wins`
       : playerSelection === "paper" && computerSelection === "scissors"
-      ? `Player : ${playerSelection} Computer : ${computerSelection} Computer Wins`
+      ? `Player : ${playerSelection}\nComputer : ${computerSelection}\nComputer Wins`
       : playerSelection === "scissors" && computerSelection === "rock"
-      ? `Player : ${playerSelection} Computer : ${computerSelection} Computer Wins`
-      : `Player : ${playerSelection} Computer : ${computerSelection} Player Wins`;
+      ? `Player : ${playerSelection}\nComputer : ${computerSelection}\nComputer Wins`
+      : `Player : ${playerSelection}\nComputer : ${computerSelection}\nPlayer Wins`;
   alert(results);
 }
-
-console.log(playRound(playerSelection, computerSelection));
